f2887307aa3dfcb57ce2c496f682e5e3b9b9d8d1
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2021-02-08 17:48:53 -0400

    listando los proyectos creados

--- cliente/src/components/layout/Sidebar.js
+++ cliente/src/components/layout/Sidebar.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import NuevoProyecto from '../proyectos/NuevoProyecto'
+import ListadoProyectos from '../proyectos/ListadoProyectos'
+
 const Sidebar = () => {
     return ( 
         <aside>
             <h1>MERN <span>Taks</span></h1>
             <NuevoProyecto/>
+
             <div className="proyectos">
+
                 <h2>tus Proyectos</h2>
+                <ListadoProyectos/>
+                
             </div>
+
         </aside>
      );
 }
