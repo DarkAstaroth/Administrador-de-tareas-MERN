00aa94ad069947b5e7d86a1ac1c2431baa364440
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2021-02-08 10:48:33 -0400

    creando el formulario para nuevos proyectos

--- cliente/src/components/layout/Sidebar.js
+++ cliente/src/components/layout/Sidebar.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import NuevoProyecto from '../proyectos/NuevoProyecto'
 const Sidebar = () => {
     return ( 
         <aside>
             <h1>MERN <span>Taks</span></h1>
+            <NuevoProyecto/>
             <div className="proyectos">
                 <h2>tus Proyectos</h2>
             </div>
