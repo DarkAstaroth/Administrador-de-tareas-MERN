f6f80c90288b46d288d0f70a1921af70adc63180
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2021-02-08 10:42:32 -0400

    primeros pasos para proyectos

---
+++ cliente/src/components/layout/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return ( 
+        <aside>
+            <h1>MERN <span>Taks</span></h1>
+            <div className="proyectos">
+                <h2>tus Proyectos</h2>
+            </div>
+        </aside>
+     );
+}
+ 
+export default Sidebar;
