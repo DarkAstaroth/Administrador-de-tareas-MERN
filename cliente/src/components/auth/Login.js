258798e4c7615fcc86c5bb336d3295d13e4e42cd
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2021-02-07 21:12:45 -0400

    state y funciones para el componente

--- cliente/src/components/auth/Login.js
+++ cliente/src/components/auth/Login.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {Link} from 'react-router-dom';
 
 const Login = () => {
+
+    // state para iniciar sesion
+
+    const [usuario, setUsuario] = useState({
+        email:'',
+        password:''
+    });
+
+    // extraer de usuario
+
+    const {email ,password} = usuario;
+
+    const onChange = e =>{
+        setUsuario({
+            ...usuario,
+            [e.target.name] : e.traget.value
+        });
+    }
+
+    // cuando el usuario quiere iniciar sesion
+
+    const onSubmit = e =>{
+        e.preventDefault();
+
+        // validar que no haya campos vacios
+
+
+        // pasarlo al action
+    }
+
     return ( 
         <div className="form-usuario">
             <div className="contenedor-form sombra-dark">
                 <h1>iniciar Sesion</h1>
 
-                <form>
+                <form
+                    onSubmit={onSubmit}
+                >
                     <div className="campo-form">
                         <label htmlFor="email">Email</label>
                         <input 
@@ -14,7 +47,8 @@ const Login = () => {
                             name="email" 
                             id="email"
                             placeholder="Tu Email"
-                            //onChange={onChange}
+                            onChange={onChange}
+                            value={email}
                         />
                     </div>
 
@@ -25,7 +59,8 @@ const Login = () => {
                             name="password" 
                             id="password"
                             placeholder="Tu Password"
-                            //onChange={onChange}
+                            onChange={onChange}
+                            value={password}
                         />
                     </div>
 
@@ -37,6 +72,7 @@ const Login = () => {
                         />
                     </div>
                 </form>
+                <Link to={'/nueva-cuenta'} className="enlace-cuenta">Obtener cuenta</Link>
             </div>
         </div>
      );
