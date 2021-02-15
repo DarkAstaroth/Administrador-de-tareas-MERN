2344917427fc969683158f9666ae0ab4fc714918
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2021-02-15 18:18:02 -0400

    Creando El servidor de express

---
+++ servidor/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+// crear el servidor
+const app = express();
+
+// puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// arrancar la app
+app.listen(PORT,()=>{
+    console.log(`El servidor esta funcionando el el puerto ${PORT}`);
+});
