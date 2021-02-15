cfe6c11aa0e940feb83a62cb27f06e14c60ff9ff
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2021-02-15 16:30:24 -0400

    Modificando la tarea

--- cliente/src/components/tareas/FormTarea.js
+++ cliente/src/components/tareas/FormTarea.js
@@ -1,4 +1,4 @@
-import React,{useContext , useState ,useEffect } from 'react';
+import React, { useContext, useState, useEffect } from 'react';
 import proyectoContext from '../../context/Proyectos/proyectoContext';
 import tareaContext from '../../context/Tareas/tareaContext';
 
@@ -9,32 +9,32 @@ const FormTarea = () => {
     // Extraer proyectos del state inicial
     const proyectosContext = useContext(proyectoContext);
     const { proyecto } = proyectosContext;
-    
+
     const tareasContext = useContext(tareaContext);
-    const { errortarea, agregarTarea , validarTarea , obtenerTareas, tareaseleccionada  } = tareasContext;
+    const { errortarea, agregarTarea, validarTarea, obtenerTareas, tareaseleccionada, actualizarTarea } = tareasContext;
 
     // Effect que detecta si hay una tarea seleccionada
     useEffect(() => {
-        if(tareaseleccionada !== null){
+        if (tareaseleccionada !== null) {
             setTarea(tareaseleccionada);
-        }else{
+        } else {
             setTarea({
-                nombre:''
+                nombre: ''
             })
         }
     }, [tareaseleccionada])
-    
+
     // state del formulario 
     const [tarea, setTarea] = useState({
-        nombre :''
+        nombre: ''
     })
 
     // extraer el nombre del proyecto
-    const {nombre} = tarea
+    const { nombre } = tarea
 
     // Si no hay proyecto seleccionado
 
-    if(!proyecto) return null;
+    if (!proyecto) return null;
 
     const [proyectoActual] = proyecto;
 
@@ -43,45 +43,51 @@ const FormTarea = () => {
     const handleChange = e => {
         setTarea({
             ...tarea,
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         })
     }
 
     const onSubmit = e => {
         e.preventDefault();
-        
+
 
         // Validar 
-        if(nombre.trim() === ''){
+        if (nombre.trim() === '') {
             validarTarea();
             return;
         }
 
-
-        // agregar la nueva tarea al state de tareas
-        tarea.proyectoId = proyectoActual.id;
-        tarea.estado = false;
-        agregarTarea(tarea);
+        // Revisar si es edicion o si es nueva tarea
+        if (tareaseleccionada === null) {
+            //tarea nueva
+            // agregar la nueva tarea al state de tareas
+            tarea.proyectoId = proyectoActual.id;
+            tarea.estado = false;
+            agregarTarea(tarea);
+        } else {
+            // Actualizar tarea existente
+            actualizarTarea(tarea);
+        }
 
         // Obtener y filtrar las tareas del proyectoabtual
         obtenerTareas(proyectoActual.id);
 
         // reiniciar el form
         setTarea({
-            nombre : ''
+            nombre: ''
         })
     }
 
 
-    return ( 
+    return (
         <div className="formulario">
             <form
                 onSubmit={onSubmit}
             >
                 <div className="contenedor-input">
-                    <input 
-                        type="text" 
-                        name="nombre" 
+                    <input
+                        type="text"
+                        name="nombre"
                         className="input-text"
                         placeholder="Nombre Tarea..."
                         value={nombre}
@@ -90,16 +96,16 @@ const FormTarea = () => {
                 </div>
 
                 <div className="contenedor-input">
-                    <input 
-                        type="submit"  
+                    <input
+                        type="submit"
                         className="btn btn-primario btn-submit btn-block"
                         value={tareaseleccionada ? 'Editar Tarea' : 'Agregar tarea'}
                     />
                 </div>
             </form>
-            {errortarea ? <p className="mensaje error">El nombre de la tarea es obligatorio</p>:null}
+            {errortarea ? <p className="mensaje error">El nombre de la tarea es obligatorio</p> : null}
         </div>
-     );
+    );
 }
- 
+
 export default FormTarea;
--- cliente/src/context/Tareas/tareaReducer.js
+++ cliente/src/context/Tareas/tareaReducer.js
@@ -4,42 +4,46 @@ import {
     VALIDAR_TAREA,
     ELIMINAR_TAREA,
     ESTADO_TAREA,
-    TAREA_ACTUAL
+    TAREA_ACTUAL,
+    ACTUALIZAR_TAREA
 } from '../../types/index'
 
-export default (state , action) => {
+export default (state, action) => {
     switch (action.type) {
         case TAREAS_PROYECTO:
             return {
-                ...state,   
-                tareasproyecto : state.tareas.filter(tarea=> tarea.proyectoId === action.payload)
+                ...state,
+                tareasproyecto: state.tareas.filter(tarea => tarea.proyectoId === action.payload)
             }
         case AGREGAR_TAREA:
             return {
                 ...state,
-                tareas:[action.payload,...state.tareas],
-                errortarea:false
+                tareas: [action.payload, ...state.tareas],
+                errortarea: false
             }
         case VALIDAR_TAREA:
             return {
                 ...state,
-                errortarea:true
+                errortarea: true
             }
         case ELIMINAR_TAREA:
             return {
                 ...state,
-                tareas : state.tareas.filter(tarea=>tarea.id !== action.payload)
+                tareas: state.tareas.filter(tarea => tarea.id !== action.payload)
             }
-        case ESTADO_TAREA :
+        case ACTUALIZAR_TAREA:
+        case ESTADO_TAREA:
             return {
                 ...state,
-                tareas: state.tareasproyecto.map(tarea=>tarea.id ===action.payload ? action.payload : tarea )
+                tareas: state.tareas.map(tarea => tarea.id === action.payload.id ? action.payload : tarea)
             }
         case TAREA_ACTUAL:
             return {
                 ...state,
                 tareaseleccionada: action.payload
             }
+        
+
         default:
             return state
     }
