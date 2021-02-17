066f5d83656176fc0f079e6c96d7aed1345760b5
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2021-02-16 21:23:00 -0400

    Insertando usuarios en la base de datos

--- servidor/controllers/usuarioController.js
+++ servidor/controllers/usuarioController.js
@@ -1,3 +1,18 @@
-exports.crearUsuario =(req,res)=>{
-    console.log(req.body);
+const Usuario = require('../models/Usuario')
+
+exports.crearUsuario = async (req,res)=>{
+    try {
+        let usuario;
+        // crea el nuevo usuario
+        usuario = new Usuario(req.body);
+
+        // guardar usuario
+        await usuario.save();
+
+        // mensaje de Confirmacion
+        res.send('usuario creado correctamente');
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Hubo un error');
+    }    
 }
