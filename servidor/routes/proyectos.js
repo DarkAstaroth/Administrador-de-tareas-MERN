b0cb1ce7b04445fa156ad542dae3355e79a0a8dd
Manuel <mninahuanca4@gmail.com>
2021-03-06 23:21:53 -0400

    validacion de los proyectos

--- servidor/routes/proyectos.js
+++ servidor/routes/proyectos.js
@@ -2,12 +2,20 @@ const express = require('express');
 const router = express.Router();
 const proyectoController = require('../controllers/proyectoController');
 const auth = require('../middleware/auth');
+const { check } = require('express-validator');
 
 // Crea un usuario
 // api/proyectos
 router.post('/',
     auth,
+    [
+        check('nombre', 'el nombre del proyecto es obligatorio').not().isEmpty()
+    ],
+    proyectoController.crearProyecto
+);
+
+router.get('/',
+    auth,
     proyectoController.crearProyecto
 )
-
 module.exports = router;
