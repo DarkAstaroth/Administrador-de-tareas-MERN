cf40c5048024ba7d23ecc219f1aaa711b015b701
Victor Manuel Ninahuanca Ayala <mninahuanca4@gmail.com>
2021-03-06 20:48:44 -0400

    Verificando si el usuario esta autenticado para crear nuevos proyectos

--- servidor/routes/proyectos.js
+++ servidor/routes/proyectos.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const proyectoController = require('../controllers/proyectoController');
+const auth = require('../middleware/auth');
 
 // Crea un usuario
 // api/proyectos
 router.post('/',
+    auth,
     proyectoController.crearProyecto
 )
 
