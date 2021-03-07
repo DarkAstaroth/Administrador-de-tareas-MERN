cb7d0e3f3d816058b89b2488a155932fe4fcd7e6
Manuel <mninahuanca4@gmail.com>
2021-03-07 17:19:02 -0400

    insertando tareas y el proyecto al que pertenecen

--- servidor/controllers/tareaController.js
+++ servidor/controllers/tareaController.js
@@ -9,4 +9,30 @@ exports.creaTarea = async (req, res) => {
     if (!errores.isEmpty()) {
         return res.status(400).json({ errores: errores.array() })
     }
+
+    // Extraer el proyecto y comprobar si existe
+
+    try {
+
+        const { proyecto } = req.body;
+
+        const existeProyecto = await Proyecto.findById(proyecto);
+        if (!existeProyecto) {
+            return res.status(404).send('Proyecto no encontrado');
+        }
+
+        // Revisar si el proyecto actual pertenece al usuario autenticado
+        if (existeProyecto.creador.toString() !== req.usuario.id) {
+            return res.status(401).json({ msg: "No autorizado" })
+        }
+
+        // Creamos la tarea
+        const tarea = new Tarea(req.body);
+        await tarea.save();
+        res.json({ tarea });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).send('Hubo un error')
+    }
 }
