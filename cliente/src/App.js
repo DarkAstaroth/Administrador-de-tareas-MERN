06884eb7159ac09c3de7d7812d596e4f768396d4
Manuel <mninahuanca4@gmail.com>
2021-03-08 12:53:54 -0400

    Creando el state y provider de autenticacion

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -6,19 +6,23 @@ import Proyectos from './components/proyectos/Proyectos';
 import ProyectoState from './context/Proyectos/ProyectoState';
 import TareaState from './context/Tareas/tareaState';
 import AlertaState from './context/Alertas/alertaState';
+import AuthState from './context/Autenticacion/authState';
+import AuthContext from './context/Autenticacion/authContext';
 
 function App() {
   return (
     <ProyectoState>
       <TareaState>
         <AlertaState>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Login} />
-              <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-              <Route exact path="/proyectos" component={Proyectos} />
-            </Switch>
-          </Router>
+          <AuthContext>
+            <Router>
+              <Switch>
+                <Route exact path="/" component={Login} />
+                <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+                <Route exact path="/proyectos" component={Proyectos} />
+              </Switch>
+            </Router>
+          </AuthContext>
         </AlertaState>
       </TareaState>
     </ProyectoState>
