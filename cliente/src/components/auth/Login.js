bb4945adc3d2b46e09005daf10d3a715ac033ad7
Manuel <mninahuanca4@gmail.com>
2021-03-09 15:52:05 -0400

    validando el inicio de sesion

--- cliente/src/components/auth/Login.js
+++ cliente/src/components/auth/Login.js
@@ -1,42 +1,55 @@
-import React,{useState} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import AlertaContext from '../../context/Alertas/alertaContext';
+import AuthContext from '../../context/Autenticacion/authContext';
 
 const Login = () => {
 
-    // state para iniciar sesion
 
+    // Extraer los valores del context
+    const alertaContext = useContext(AlertaContext);
+    const { alerta, mostrarAlerta } = alertaContext;
+
+    const authContext = useContext(AuthContext);
+    const { mensaje, autenticado, iniciarSesion } = authContext;
+
+    // state para iniciar sesion
     const [usuario, setUsuario] = useState({
-        email:'',
-        password:''
+        email: '',
+        password: ''
     });
 
     // extraer de usuario
 
-    const {email ,password} = usuario;
+    const { email, password } = usuario;
 
-    const onChange = e =>{
+    const onChange = e => {
         setUsuario({
             ...usuario,
-            [e.target.name] : e.target.value
+            [e.target.name]: e.target.value
         });
     }
 
     // cuando el usuario quiere iniciar sesion
 
-    const onSubmit = e =>{
+    const onSubmit = e => {
         e.preventDefault();
 
         // validar que no haya campos vacios
-
-        // Password minimo de 6 caracteres
-
-        // Los 2 passwords sean iguales
+        if (email.trim() === "" || password.trim() === "") {
+            mostrarAlerta('Todos los campos son obligatorios', 'alerta-error');
+        }
 
         // pasarlo al action
+        iniciarSesion({ email, password });
     }
 
-    return ( 
+    return (
         <div className="form-usuario">
+            { alerta
+                ? (<div className={`alerta ${alerta.categoria}`}>{alerta.msg}</div>)
+                : null
+            }
             <div className="contenedor-form sombra-dark">
                 <h1>iniciar Sesion</h1>
 
@@ -45,9 +58,9 @@ const Login = () => {
                 >
                     <div className="campo-form">
                         <label htmlFor="email">Email</label>
-                        <input 
-                            type="email" 
-                            name="email" 
+                        <input
+                            type="email"
+                            name="email"
                             id="email"
                             placeholder="Tu Email"
                             onChange={onChange}
@@ -57,9 +70,9 @@ const Login = () => {
 
                     <div className="campo-form">
                         <label htmlFor="password">Password</label>
-                        <input 
-                            type="password" 
-                            name="password" 
+                        <input
+                            type="password"
+                            name="password"
                             id="password"
                             placeholder="Tu Password"
                             onChange={onChange}
@@ -68,9 +81,9 @@ const Login = () => {
                     </div>
 
                     <div className="campo-form">
-                        <input 
+                        <input
                             type="submit"
-                            className="btn btn-primario btn-block" 
+                            className="btn btn-primario btn-block"
                             value="Iniciar Sesion"
                         />
                     </div>
@@ -78,7 +91,7 @@ const Login = () => {
                 <Link to={'/nueva-cuenta'} className="enlace-cuenta">Obtener cuenta</Link>
             </div>
         </div>
-     );
+    );
 }
- 
+
 export default Login;
--- cliente/src/context/Autenticacion/authState.js
+++ cliente/src/context/Autenticacion/authState.js
@@ -76,6 +76,26 @@ const AuthState = props => {
     }
 
 
+    // cuando el usuario inicia sesion
+    const iniciarSesion = async datos => {
+        try {
+            const respuesta = await clienteAxios.post('/api/auth', datos);
+            console.log(respuesta);
+        } catch (error) {
+            console.log(error.response.data.msg)
+            const alerta = {
+                msg: error.response.data.msg,
+                categoria: 'alerta-error'
+            }
+
+            dispatch({
+                type: LOGIN_ERROR,
+                payload: alerta
+            })
+        }
+    }
+
+
 
     return (
         <AuthContext.Provider
@@ -84,7 +104,8 @@ const AuthState = props => {
                 autenticado: state.autenticado,
                 usuario: state.usuario,
                 mensaje: state.mensaje,
-                registrarUsuario
+                registrarUsuario,
+                iniciarSesion
             }}
         >
             {props.children}
