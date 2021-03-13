96d3f1a98779a935e11b283a9ee381c6edc45cd1
Manuel <mninahuanca4@gmail.com>
2021-03-13 01:08:30 -0400

    Eliminando tareas

--- cliente/src/components/tareas/Tarea.js
+++ cliente/src/components/tareas/Tarea.js
@@ -15,7 +15,7 @@ const Tarea = ({ tarea }) => {
 
     // Funcion que se ejecuta cuando el usuario presiona el btn de eliminar tarea
     const tareaEliminar = id=>{
-        eliminarTarea(id);
+        eliminarTarea(id,proyectoActual._id);
         obtenerTareas(proyectoActual.id);
     }
 
@@ -65,7 +65,7 @@ const Tarea = ({ tarea }) => {
                 <button
                     type="button"
                     className="btn btn-secundario"
-                    onClick={()=>tareaEliminar(tarea.id)}
+                    onClick={()=>tareaEliminar(tarea._id)}
                 >Eliminar</button>
             </div>
         </li>
